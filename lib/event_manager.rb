f0b49406810b119205dd221777ea396884c0b8a6
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-12-03 11:00:48 +0100

    Start working on finding best hours

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -19,6 +20,11 @@ def legislators_by_zipcode(zip)
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
+end
+
+def hour_from_registration_date(date)
+  date_array = date.split
+  hour = Time.parse(date_array[1]).hour
 end
 
 def clean_phone_number(phone_number)
@@ -55,14 +61,22 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+best_hour = []
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
   phone_number = clean_phone_number(row[:homephone])
-
+  registration_hour = hour_from_registration_date(row[:regdate])
+  best_hour.push(registration_hour)
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id,form_letter)
 end
+overview = Hash.new(0)
+best_hour.each do |hour|
+  overview[hour] += 1
+end
+p overview
